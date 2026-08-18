8221dfd6fe8461613c105f0fe8aacae5342d73de
tummechatronics <tummechatronics38@gmail.com>
2026-08-18 09:03:09 +0200

    chapter 2

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,17 +1,39 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
-use std::io::Ordering
 fn main() {
     println!("Guess the number!");
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {secret_number}");
-    println!("Please input your guess.!:::::");
+    let secret_number = rand::thread_rng().gen_range(1..=10);
+    //println!("The secret number is: {secret_number}");
 
-    let mut guess = String::new(); //mutable variable guess the type String
+    loop {
+        //infinity loop
+        let mut guess = String::new(); //mutable variable guess the type String
+        println!("Please input your guess.!:::::");
+        io::stdin() //comment
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        //translate guess from string to u32
 
-    io::stdin() //comment
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("you guessed: {guess}");
+        //let guess: u32 = guess.trim().parse().expect("Please type a number!"); -> crashing on error
+        //handling the error -> match || crashing on error -> expect
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            //_, is a catch-all value
+            Err(_) => {
+                println!("wrong input");
+                println!("ijshfishf");
+                continue;
+            }
+        };
+        println!("you guessed: {guess}");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            }
+        }
+    }
 }
